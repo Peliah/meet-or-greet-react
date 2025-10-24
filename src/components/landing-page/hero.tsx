84be0171f81f01ee34d76741d8c8bbce0ce6dd08
Peliah <pelepoupa@gmail.com>
2025-10-24 11:42:12 +0100

    feat: fixed the hero

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import wavyBackground from '../../assets/wave.svg';
+import wavyBackground from '../../assets/wave-black.svg';
+import { Button } from '../ui/button';
 
 const Hero: React.FC = () => {
     return (
-        <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
+        <section className="relative min-h-screen flex w-full">
             <div className="absolute inset-0 z-0">
                 <img
                     src={wavyBackground}
@@ -11,10 +12,24 @@ const Hero: React.FC = () => {
                     className="w-full h-full object-cover"
                     aria-hidden="true"
                 />
-                {/* <div className="absolute inset-0 bg-gradient-to-br from-primary/10 via-transparent to-primary/5" /> */}
             </div>
+            <div className="max-w-7xl mx-auto z-10 flex justify-between w-full px-20 gap-10">
+                <div className="w-2/3 text-white flex flex-col gap-4 items-start justify-center">
+                    <h1 className="text-9xl font-black text-foreground text-slate-200 leading-tight">
+                        Doo-shen: Sell Tickets, Grow Events.
+                    </h1>
+                    <p className="text-lg text-foreground text-slate-200 leading-relaxed">
+                        Doo-shen is a ticket management system that allows you to manage your tickets efficiently and effectively.
+                    </p>
+                    <Button className="bg-white">
+                        Get Started
+                    </Button>
 
+                </div>
+                <div className="w-1/3">
 
+                </div>
+            </div>
         </section>
     );
 };
